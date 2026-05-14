ab9ccae7f7fe94280e544e28f3150841dbb8ee62
CoderXYZ14 <s1404islam@gmail.com>
2026-05-14 19:45:08 +0530

    Env varibale updated

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,8 @@
 export const AUTH_SERVICE_URL =
   process.env.NEXT_PUBLIC_AUTH_SERVICE_URL ?? "http://localhost:3001";
 
-export const HOME_URL = process.env.HOME_URL ?? "http://localhost:3000";
+export const HOME_URL =
+  process.env.NEXT_PUBLIC_HOME_URL ?? "http://localhost:3000";
 
 export const APP_URL = process.env.APP_URL ?? "http://localhost:3002";
 
@@ -10,8 +11,7 @@ export const CLI_BACKEND_URL =
 
 export const MONGODB_URI = process.env.MONGODB_URI ?? "";
 
-export const JWT_SECRET =
-  process.env.JWT_SECRET ?? process.env.NEXTAUTH_SECRET ?? "";
+export const JWT_SECRET = process.env.JWT_SECRET ?? "";
 
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
